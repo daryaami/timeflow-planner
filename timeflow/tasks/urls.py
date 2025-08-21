b8f651f17b500516d6f497c29e5acd4dfe9fd9fc
Darya <104630608+daryaami@users.noreply.github.com>
2025-08-21 11:49:54 +0800

    fix categories view

--- timeflow/tasks/urls.py
+++ timeflow/tasks/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from .apis import TaskViewSet, CategoryViewSet, TimeLogViewSet
 
 router = DefaultRouter()
-router.register(r'', TaskViewSet, basename='task')
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'timelogs', TimeLogViewSet, basename='timelog')
+router.register(r'', TaskViewSet, basename='task')
 
 urlpatterns = [
     path('', include(router.urls)),
